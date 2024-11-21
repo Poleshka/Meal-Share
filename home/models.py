b8795d52a3fa023c4cffe05c0673f87955898b7f
Paulina Adams <pstafska@gmail.com>
2024-11-21 14:46:55 +0000

    Create home app and base template

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Home(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content=models.TextField
--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request,*args,**kwargs):
-    return render(request, 'home/index.html',{})
+    return render(request, 'home/index.html',{"home":home},)
