55b6a8bf6d5c652858c1029bc1c706e0ea748fab
Paulina Adams <pstafska@gmail.com>
2024-12-05 13:19:35 +0000

    Add:Recipe/Comment models to admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe, Comment
 
 # Register your models here.
-# @admin.register(Recipe)
+admin.site.register(Recipe)
+admin.site.register(Comment)
 
 
