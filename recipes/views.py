d31b89477419c75f6531ef6b3b93e6cd5c2f71c6
Paulina Adams <pstafska@gmail.com>
2024-12-12 14:18:23 +0000

    Added styles to login/out

--- recipes/views.py
+++ recipes/views.py
@@ -70,6 +70,11 @@ class AddRecipe(LoginRequiredMixin, CreateView):
     form_class =RecipeForm
     #fields = ['title', 'ingredients', 'description']  replace these with the fields form your model. 
     success_url = reverse_lazy('recipes')
+
+    def get_success_url(self):
+        # return reverse('recipe_detail', kwargs={'pk': self.object.id})
+        messages.success(self.request, "Your recipe been added!")
+        return reverse('recipe_detail', kwargs={'pk': self.object.id})
     
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -82,6 +87,8 @@ class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Recipe
 
     def get_success_url(self):
+        # return reverse('recipe_detail', kwargs={'pk': self.object.id})
+        messages.success(self.request, "Your recipe has been updated successfully!")
         return reverse('recipe_detail', kwargs={'pk': self.object.id})
 
     def test_func(self):
@@ -91,6 +98,10 @@ class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """DElete Recipe"""
     model= Recipe
     success_url= '/recipes/'
+
+    def get_success_url(self):
+        messages.success(self.request, "Your recipe has been deleted successfully!")
+        return self.success_url
 
     def test_func(self):
         return self.request.user == self.get_object().user
